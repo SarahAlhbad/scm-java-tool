5a73024218aa92e8759241377851072b048fb21b
SarahAlhbad <sarahalhbbad@gmail.com>
2025-04-22 01:06:38 +0300

    first upladed on main branch

---
+++ src/passwordgenerator/PasswordGenerator.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package passwordgenerator;
+
+/**
+ *
+ * @author mohAlyosofi
+ */
+import java.security.SecureRandom;
+import java.util.Scanner;
+import javax.xml.transform.Source;
+
+public class PasswordGenerator {
+
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String CHAR_POOL = UPPERCASE + LOWERCASE + DIGITS;
+
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHAR_POOL.length());
+            password.append(CHAR_POOL.charAt(index));
+        }
+
+        return password.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter desired password length: ");
+        int length = scanner.nextInt();
+
+        String password = generatePassword(length);
+        System.out.println("Generated Password: " + password);
+    }
+    
+}
+
